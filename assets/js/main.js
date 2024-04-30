9a9978f6e7569a262063e12bc8bcd69cd8e697de
Parth Saliya <parthsaliya63@gmail.com>
2024-04-30 17:48:50 +0530

    ok
    ok

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+        spaceBetween: 10,
+    direction: 'horizontal',
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
